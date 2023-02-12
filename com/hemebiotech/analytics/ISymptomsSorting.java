a74dd03c5a81511fb3e42f47cbdcbfb7b87e30e5
Jean-Roch Tomaso <9r7r96zfc397-student@anonymous-codio.com>
2023-02-12 15:27:16 +0000

    writer implementation

--- com/hemebiotech/analytics/ISymptomsSorting.java
+++
@@ -1,8 +0,0 @@
-package com.hemebiotech.analytics;
-
-import java.util.List;
-import java.util.TreeMap;
-
-public interface ISymptomsSorting {
-    public TreeMap<String, Integer> sortingSymptoms(List<String> symtomList);
-}
